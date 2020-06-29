103ca5fad065772c85ad58637d8348d60f59ec91
zjh <2285950891@qq.com>
2020-06-29 23:03:27 +0800

    path使用正则表达式match

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type apiInfo struct {
@@ -16,23 +17,35 @@ type apiRegister struct {
 }
 
 func (p *apiRegister) Add(info apiInfo) {
-	//还需要判断是否重复
+	for _, apiInfo := range p.apis {
+		if apiInfo.path == info.path {
+			log.Fatal("path重复")
+		}
+	}
 	p.apis = append(p.apis, info)
 }
 
 func (p *apiRegister) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	notFount := true
 	for _, apiInfo := range p.apis {
-		if r.URL.Path == apiInfo.path {
+		matched, err := regexp.MatchString(apiInfo.path, r.URL.Path)
+		if err != nil {
+			log.Fatal("regexp.MatchString: ", err)
+		}
+		if matched {
 			apiInfo.handler(w, r)
+			notFount = false
 		}
 	}
+	if notFount {
+		http.NotFound(w, r)
+	}
 	//http.NotFound(w, r)
-	//return
 }
 
 func main() {
 	aI := apiInfo{
-		path:    "/zjh",
+		path:    "/zjh/[0-9]*$",
 		handler: indexHandler,
 	}
 	aR := &apiRegister{}
