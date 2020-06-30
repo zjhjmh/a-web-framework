db84488537b69dd39938f83c1bbd6cb7782e54be
zjh <2285950891@qq.com>
2020-06-30 19:08:15 +0800

    数据库

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module web-zjh
 
 go 1.13
+
+require github.com/lib/pq v1.7.0
--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"regexp"
 )
 
 type apiInfo struct {
-	path string
+	path    string
 	handler func(w http.ResponseWriter, r *http.Request)
 }
 
@@ -40,17 +42,56 @@ func (p *apiRegister) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if notFount {
 		http.NotFound(w, r)
 	}
-	//http.NotFound(w, r)
+}
+
+const (
+	host     = "47.114.110.168"
+	port     = 5432
+	user     = "micl"
+	password = "miclpwd"
+	dbname   = "zjhmgeadata"
+)
+
+type auth_group struct {
+	id   int
+	name string
 }
 
 func main() {
 	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
 	log.Print("服务器启动")
+	//数据库测试
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	//db, err := sql.Open("postgres", "user=micl password=miclpwd dbname=zjhmgeadata sslmode=disable")
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("数据库连接成功")
+
+	stmt, err := db.Prepare("INSERT INTO auth_group (id,name) VALUES ($1,$2) RETURNING id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	agtest := auth_group{
+		id:   1,
+		name: "zjh",
+	}
+	res, err := stmt.Exec(agtest.id, agtest.name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(res)
 	aR := &apiRegister{}
 	for _, url := range urls {
 		aR.Add(url)
 	}
-	err := http.ListenAndServe(":8000", aR)
+	err = http.ListenAndServe(":8000", aR)
 	if err != nil {
 		log.Fatal("listenAndServe: ", err)
 	}
